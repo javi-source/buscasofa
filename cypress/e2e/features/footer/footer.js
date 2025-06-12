c0041d0982b8e1899d3488567fa690ea99accb29
javi-source <gjaviunir1@gmail.com>
2025-06-12 19:32:35 +0200

    Update footer.js

--- cypress/e2e/features/footer/footer.js
+++ cypress/e2e/features/footer/footer.js
@@ -1,1 +1,11 @@
+import { Given, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+
+Given("el usuario está en la página principal", () => {
+  cy.visit("/");
+});
+
+Then("debería ver el nombre {string}", (nombre) => {
+  cy.get("footer").contains(String(nombre)).should("exist");
+});
 
