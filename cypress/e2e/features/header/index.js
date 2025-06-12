882f71757f28beec9ca6954c48805052402c31ea
javi-source <gjaviunir1@gmail.com>
2025-06-12 20:33:06 +0200

    Update index.js

--- cypress/e2e/features/header/index.js
+++ cypress/e2e/features/header/index.js
@@ -17,8 +17,8 @@ Then('debería ver el enlace "Mapa"', () => {
   cy.get('a.mapa').should('be.visible').and('contain', 'Mapa');
 });
 
-Then('debería ver el enlace "Quienes somos"', () => {
-  cy.get('a.about').should('be.visible').and('contain', 'Quienes somos');
+Then('debería ver el enlace "Acerca de"', () => {
+  cy.get('a.about').should('be.visible').and('contain', 'Acerca de');
 });
 
 Then('debería ver el enlace "Login"', () => {
