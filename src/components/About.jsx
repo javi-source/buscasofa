1ec083a5b89a36f0655e28b48b7521da064eebd1
NachoCM1995 <nachocastejon@gmail.com>
2025-06-13 13:08:26 +0200

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     <div className="about-container">
         <h1>Acerca de nosotros</h1>
         <div id="info">
-            Somos el equipo nº 23
+            Somos el equipo nº 15
         </div>
     </div>
 
